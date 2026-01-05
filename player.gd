1cd06b602add4d8d92b51f4b6e41b6a0e22d2a2f
Arwildo <arwildo@gmail.com>
2026-01-05 15:03:49 +0700

    feat(slime): enemy akan mati ketika kenak attackbox player

--- player.gd
+++ player.gd
@@ -36,3 +36,17 @@ func _process(delta):
 func _physics_process(delta):
 	state.physics_update(delta)
 	move_and_slide()
+
+
+func _on_animated_sprite_2d_frame_changed() -> void:
+	var animated_sprite := $AnimatedSprite2D
+	var attack_box := $AttackBox
+	
+	if animated_sprite.animation.begins_with("serang"):
+		if animated_sprite.frame == 1:
+			attack_box.monitoring = true
+		else:
+			attack_box.monitoring = false
+	else:
+		attack_box.monitoring = false
+	print(attack_box.monitoring)
--- slime.gd
+++ slime.gd
@@ -1,6 +1,29 @@
 extends CharacterBody2D
 
 @onready var sprite := $AnimatedSprite2D
+@onready var hitbox := $Hitbox
+
+var sudah_mati = false
 
 func _ready():
 	sprite.play("idle")
+	hitbox.area_entered.connect(kena_serang)
+
+func kena_serang(area):
+	if sudah_mati:
+		return
+
+	if area.name == "AttackBox":
+		musuh_mati()
+
+func musuh_mati():
+	sudah_mati = true
+
+	hitbox.monitoring = false
+	set_physics_process(false)
+	set_process(false)
+
+	sprite.play("mati")
+
+	await get_tree().create_timer(0.4).timeout
+	queue_free()
